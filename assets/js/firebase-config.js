fb8b9f8b24ce99278f93b686831f0d745728d94f
Nicolas-Batalha <nicolasbatalha231@gmail.com>
2026-07-23 12:38:53 -0300

    0.6

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,10 +1,11 @@
 export const firebaseConfig = Object.freeze({
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  apiKey: "AIzaSyA1h3JPTR6QsX_8yLVICxODbNs6tGTwR2g",
+  authDomain: "ofertas-que-valem.firebaseapp.com",
+  projectId: "ofertas-que-valem",
+  storageBucket: "ofertas-que-valem.firebasestorage.app",
+  messagingSenderId: "1067773544050",
+  appId: "1:1067773544050:web:b4d46aa9891e6b93a824eb",
+  measurementId: "G-29DW09E1MP",
   adminUid: "kpU6X9tsXATbyuUw7fje269z4EL2"
 });
 
